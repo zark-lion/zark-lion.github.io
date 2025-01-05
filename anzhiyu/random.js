9d65917910b1b98794957f081cb797c426c94dd0
wf su <wenfeng.1982.su@gmail.com>
2025-01-05 16:06:40 +0800

    Site updated: 2025-01-05 16:06:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/26/hello-world/","2024/12/26/这是新的/"];function toRandomPost(){
+var posts=["2024/12/26/hello-world/","2024/12/26/text1/","2024/12/26/title/","2024/12/26/这是新的/","2025/01/05/TEST/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

d4fb4830def846c39e26048b63233dfc417ab53d
wf su <wenfeng.1982.su@gmail.com>
2025-01-08 18:26:14 +0800

    Site updated: 2025-01-08 18:26:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/26/hello-world/","2025/01/05/new/","2025/01/05/大学日志/","2024/12/26/这是新的/"];function toRandomPost(){
+var posts=["2024/12/26/hello-world/","2025/01/05/大学日志/","2024/12/26/这是新的/","2025/01/05/new/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
